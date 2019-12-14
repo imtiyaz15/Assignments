15429428f66f6d0c1b71aad9fee822038f5c175a
imtiyaz15 <mr.imtiyaz15@gmail.com>
2019-12-14 18:18:40 +0530

    Task 5 added to repo

---
+++ Task_5/DataPassingToView/MyCollectionViewCell.swift
@@ -0,0 +1,18 @@
+//
+//  MyCollectionViewCell.swift
+//  Task_5
+//
+//  Created by Thanos on 22/11/19.
+//  Copyright © 2019 Thanos. All rights reserved.
+//
+
+import UIKit
+
+class MyCollectionViewCell: UICollectionViewCell {
+    
+    
+    @IBOutlet weak var cell_Img: UIImageView!
+    
+    @IBOutlet weak var cell_Label: UILabel!
+    
+}
