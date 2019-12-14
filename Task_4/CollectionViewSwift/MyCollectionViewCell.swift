b004b8b2ee3045503acf579a54e1bef4b808eff1
imtiyaz15 <mr.imtiyaz15@gmail.com>
2019-12-14 18:17:03 +0530

    Task 4 added to repo

---
+++ Task_4/CollectionViewSwift/MyCollectionViewCell.swift
@@ -0,0 +1,17 @@
+//
+//  MyCollectionViewCell.swift
+//  Task_4
+//
+//  Created by Brahmastra on 20/11/19.
+//  Copyright © 2019 Brahmastra. All rights reserved.
+//
+
+import UIKit
+
+class MyCollectionViewCell: UICollectionViewCell {
+    
+    @IBOutlet weak var img: UIImageView!
+    @IBOutlet weak var captionLabel: UILabel!
+    
+    
+}
